2e9d170c8d9f0944cb8a884dc694420eae3fa06d
Zubair <zashraf>
2017-05-10 03:17:03 -0400

    conf changes to run along with cuckoo 2.0, added script query inetsim log

--- utils/queryInetSimLog.sh
+++ utils/queryInetSimLog.sh
@@ -1,4 +1,4 @@
-sudo grep -iE "info" /var/log/inetsim/service.log  | grep "$1" | grep -E "info.*Request" | grep -vf utils/networkWhiteList.txt | cut -f 6-12 -d : |  sed -e "s/\./\.\./"
-sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vf utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
-sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vf utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
+sudo grep -iE "info" /var/log/inetsim/service.log  | grep "$1" | grep -E "info.*Request" | grep -vf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 6-12 -d : |  sed -e "s/\./\.\./"
+sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
+sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
 
