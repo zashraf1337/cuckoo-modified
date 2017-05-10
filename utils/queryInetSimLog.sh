65247c038fb21cf8a9ec308ed224cc29f42d3f5f
Zubair <zashraf>
2017-05-10 03:39:30 -0400

    conf changes to run along with cuckoo 2.0, added script query inetsim log

--- utils/queryInetSimLog.sh
+++ utils/queryInetSimLog.sh
@@ -1,6 +1,4 @@
-echo $1
-sudo grep -iE "info" /var/log/inetsim/service.log  | grep "$1" | grep -E "info.*Request" 
-sudo grep -iE "info" /var/log/inetsim/service.log  | grep "$1" | grep -E "info.*Request" | grep -vEf ~/cuckoo-modified/utils/networkWhiteList.txt
+echo querying $1
 sudo grep -iE "info" /var/log/inetsim/service.log  | grep "$1" | grep -E "info.*Request" | grep -vEf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 6-12 -d : |  sed -e "s/\./\.\./"
 sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vEf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
 sudo grep -iE "dns_.*recv" /var/log/inetsim/service.log  | grep "$1" | grep -vEf ~/cuckoo-modified/utils/networkWhiteList.txt | cut -f 4-12 -d : | sed -e "s/\./\.\./"
